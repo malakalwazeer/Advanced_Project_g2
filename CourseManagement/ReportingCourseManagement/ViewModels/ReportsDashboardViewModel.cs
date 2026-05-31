09b45e1abc5063f85176586befe1c723447326a1
malakalwazeer <malakalwazeer12@gmail.com>
2026-05-31 21:17:26 +0300

    Add charts to reporting dashboard

---
+++ CourseManagement/ReportingCourseManagement/ViewModels/ReportsDashboardViewModel.cs
@@ -0,0 +1,18 @@
+﻿using ReportingCourseManagement.Dtos;
+namespace ReportingCourseManagement.ViewModels
+{
+    public class ReportsDashboardViewModel
+    {
+
+        //This ViewModel only groups API data for the dashboard page.
+        public List<EnrollmentByCourseReportDto> EnrollmentByCourse { get; set; } = new();
+
+        public List<EnrollmentByCategoryReportDto> EnrollmentByCategory { get; set; } = new();
+
+        public List<InstructorWorkloadReportDto> InstructorWorkload { get; set; } = new();
+
+        public RevenueSummaryReportDto RevenueSummary { get; set; } = new();
+
+        public List<CertificationCompletionReportDto> CertificationCompletion { get; set; } = new();
+    }
+}
